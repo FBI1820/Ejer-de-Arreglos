ccfe01f4ffe287779485ae35c186be71220a34d9
Mirai <158864225+Mirai1602@users.noreply.github.com>
2025-09-19 10:39:51 -0600

    Add files via upload

---
+++ SegundoEjercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+
+     
+        
+        Console.WriteLine("Ingrese un número entero: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("El valor ingresado es: " + x);
+        static void Intercambiar(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("Después del intercambio, el valor de x es: " + x);
+        } 
+
+    }
+
+   
+
+    
+
+   
+}
