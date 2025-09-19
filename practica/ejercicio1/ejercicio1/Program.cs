1b54fa2738cc38d3bccd7cae295dce842a651455
gabi <gabiespr07@gmail.com>
2025-09-19 10:47:10 -0600

    Add files via upload

---
+++ practica/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, res;
+            Console.WriteLine("ingrese un numero entero:");
+            num = Convert.ToInt32(Console.ReadLine());
+            res = verificar(num);
+
+            if (res == 0)
+            {
+                Console.WriteLine("es par");
+            }
+            else
+            {
+                Console.WriteLine("es impar");
+            }
+            Console.ReadKey();
+
+
+        }
+
+        static int verificar(int num)
+        {
+          if (num % 2 == 0)
+            {
+                return 0;
+            }
+          else
+            { 
+                return 1;
+            }
+        }
+    }
+}
---
+++ practica/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio, precioFinal, iva;
+            Console.WriteLine("ingrese el precio total:");
+            precio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"el precio total es: {precio}");
+            iva = calculoiva(precio);
+            precioFinal = total(precio, iva);
+
+            Console.WriteLine($"IVA (15%) es:  {iva}");
+            Console.WriteLine($"Precio con IVA incluido:   {precioFinal}");
+
+            Console.ReadKey();
+
+        }
+        static double calculoiva(double precio)
+        {
+            double iva = precio * 0.15;
+            return iva;
+            
+        }
+        static double total(double precio, double iva)
+        {
+            return precio + iva;
+        }
+    }
+}
